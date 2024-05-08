ab5b68051d1711acf85d546c4095ce33bcb4d3c7
vitor-dornela <vitor.dornela.m@gmail.com>
2024-05-08 12:01:28 -0300

    n

--- TabelaVerdade/src/Parser.js
+++ TabelaVerdade/src/Parser.js
@@ -327,7 +327,7 @@ class Parser {
 			return z;
 		} else {
 			let s = this.token.toUpperCase();
-			if ( ! s.match(/[A-Z][0-9]*/) ) {
+			if ( ! s.match(/[A-Z]\d*/) ) {
 				let t = this.emend();
 				this.adderror( "Invalid propositional letter \""
 					  + s + "\" at position " +
