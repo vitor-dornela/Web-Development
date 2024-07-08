f79c24abf51da6077ff13cf763c0decbfcbe71a2
vitor-dornela <vitor.dornela.m@gmail.com>
2024-07-07 22:19:26 -0300

    numsec2

--- Numero Secreto/app.js
+++ Numero Secreto/app.js
@@ -1,11 +1,18 @@
 alert('Bem vindo ao jogo do número secreto!');
 let numeroSecreto = 29;
-let chute = prompt('Digite um número: ');
+let chute;
 console.log('Este foi o chute: ' + chute);
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou! ${numeroSecreto}`); // Template String
-    } else if (chute > numeroSecreto) {
-            alert(`O número secreto é menor! que ${chute}`);
-        } else {
-            alert(`O número secreto é maior! que ${chute}`);
-        }
+
+while(chute != numeroSecreto) {
+    chute = prompt('Digite um número até 100: ')
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você acertou! ${numeroSecreto}`); // Template String
+        } 
+    else if (chute > numeroSecreto) {
+        alert(`O número secreto é menor! que ${chute}`);
+    }
+    else {
+        alert(`O número secreto é maior! que ${chute}`);
+    }
+}
+
