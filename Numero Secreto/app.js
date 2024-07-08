8ece32250cc93827e54390fa705e3e79c31323e3
vitor-dornela <vitor.dornela.m@gmail.com>
2024-07-07 23:07:12 -0300

    NumSec3

--- Numero Secreto/app.js
+++ Numero Secreto/app.js
@@ -1,12 +1,15 @@
 alert('Bem vindo ao jogo do número secreto!');
-let numeroSecreto = 29;
+let numeroMaximo = 10;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo) + 1;
+console.log('Este é o número secreto: ' + numeroSecreto);
 let chute;
-console.log('Este foi o chute: ' + chute);
+let tentativas = 0;
 
 while(chute != numeroSecreto) {
-    chute = prompt('Digite um número até 100: ')
+    chute = prompt(`Digite um número entre 1 e ${numeroMaximo}: `)
+    tentativas++;
     if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou! ${numeroSecreto}`); // Template String
+        break;
         } 
     else if (chute > numeroSecreto) {
         alert(`O número secreto é menor! que ${chute}`);
@@ -15,4 +18,9 @@ while(chute != numeroSecreto) {
         alert(`O número secreto é maior! que ${chute}`);
     }
 }
+console.log(`Tentativas: ${tentativas}`);
+
+let palavraTentativas = tentativas == 1 ? 'tentativa' : 'tentativas';
+alert(`Parabéns! Você acertou o número secreto em ${tentativas} ${palavraTentativas}!`);
+
 
