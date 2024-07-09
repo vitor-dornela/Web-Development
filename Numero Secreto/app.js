55696491677101d14d4baae42cfc266a6624c5d5
vitor-dornela <vitor.dornela.m@gmail.com>
2024-07-08 23:28:24 -0300

    aula2

--- Numero Secreto/app.js
+++ Numero Secreto/app.js
@@ -1,26 +1,11 @@
-alert('Bem vindo ao jogo do número secreto!');
-let numeroMaximo = 10;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo) + 1;
-console.log('Este é o número secreto: ' + numeroSecreto);
-let chute;
-let tentativas = 0;
-
-while(chute != numeroSecreto) {
-    chute = prompt(`Digite um número entre 1 e ${numeroMaximo}: `)
-    tentativas++;
-    if (chute == numeroSecreto) {
-        break;
-        } 
-    else if (chute > numeroSecreto) {
-        alert(`O número secreto é menor! que ${chute}`);
-    }
-    else {
-        alert(`O número secreto é maior! que ${chute}`);
-    }
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
 }
-console.log(`Tentativas: ${tentativas}`);
 
-let palavraTentativas = tentativas == 1 ? 'tentativa' : 'tentativas';
-alert(`Parabéns! Você acertou o número secreto em ${tentativas} ${palavraTentativas}!`);
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 
-
+function verificarChute() {
+    console.log('Chute foi clicado');
+}
